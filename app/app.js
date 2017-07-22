4f20da33e4089f6c53fa277021fed0aae36aa3a2
Alexandr Sazanovich <alexander@denovo-digital.com>
2017-07-22 11:42:23 +0300

    import angular bootstrap

--- app/app.js
+++ app/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var myApp = angular.module('myApp', [
   'ngRoute',
+  'ui.bootstrap',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
